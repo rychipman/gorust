f9e5d751cc6902279a3d1d7d1a039d8f01056892
Ryan Chipman <ryan@ryanchipman.com>
2018-10-12 12:55:41 -0400

    Add print func to rust lib

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,5 +1,15 @@
+extern crate libc;
+use std::ffi::CStr;
 
 #[no_mangle]
 pub extern fn get_hello_string() -> String {
     format!("Hello, world!")
+}
+
+pub extern fn print_hello_string(name: *const libc::c_char) {
+    let buf_name = unsafe {
+        CStr::from_ptr(name).to_bytes()
+    };
+    let str_name = String::from_utf8(buf_name.to_vec()).unwrap();
+    println!("Hello to {} from rust!", str_name);
 }
