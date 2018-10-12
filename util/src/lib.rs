49ab6541d467bb4905f76addb1c7e73f77bbb2a0
Ryan Chipman <ryan@ryanchipman.com>
2018-10-12 13:06:47 -0400

    rustfmt extern calls

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -2,14 +2,13 @@ extern crate libc;
 use std::ffi::CStr;
 
 #[no_mangle]
-pub extern fn get_hello_string() -> String {
+pub extern "C" fn get_hello_string() -> String {
     format!("Hello, world!")
 }
 
-pub extern fn print_hello_string(name: *const libc::c_char) {
-    let buf_name = unsafe {
-        CStr::from_ptr(name).to_bytes()
-    };
+#[no_mangle]
+pub extern "C" fn print_hello_string(name: *const libc::c_char) {
+    let buf_name = unsafe { CStr::from_ptr(name).to_bytes() };
     let str_name = String::from_utf8(buf_name.to_vec()).unwrap();
     println!("Hello to {} from rust!", str_name);
 }
